76e3145e5fc96e292cf993a4f690dd32ba43c6e4
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-21 22:40:23 -0500

    Completed CarLotSimulator

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //Create a seperate class file called Car
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property
+
+        public Car() 
+        { 
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable) 
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+        public void MakeEngineNoise(string EngineNoise)
+        {
+            if (EngineNoise == null)
+            {
+                Console.WriteLine($"No noise noted from {Year} {Make} {Model}.");
+            }
+            
+            var lowercaseEngineNoise = EngineNoise.ToLower();
+            
+            if (lowercaseEngineNoise == "yes" || lowercaseEngineNoise == "y") 
+            { 
+                Console.WriteLine($"{Year} {Make} {Model} makes a unidentified noise"); 
+            }
+            else if (lowercaseEngineNoise == "no" || lowercaseEngineNoise == "n") 
+            {
+                Console.WriteLine($"No noise heard from engine of {Year} {Make} {Model}.");
+            }
+            else 
+            {
+                Console.WriteLine($"There is a noise described as {EngineNoise} from {Year} {Make} {Model} .");
+            }
+        }
+        public void MakeHonkNoise(string HonkNoise)
+        {
+            if (HonkNoise == null)
+            {
+                Console.WriteLine($"No honk test noted for {Year} {Make} {Model}.");
+            }
+
+            var lowerCaseHonk = HonkNoise.ToLower();
+
+            if (lowerCaseHonk == "yes" || lowerCaseHonk == "y")
+            {
+                Console.WriteLine($"Horn is operational for {Year} {Make} {Model}.");
+            }
+            else if (lowerCaseHonk == "no" || lowerCaseHonk == "n")
+            {
+                Console.WriteLine($"Horn not functioning in {Year} {Make} {Model}.");
+            }
+            else
+            {
+                Console.WriteLine($"The horn function is described as {HonkNoise} for {Year} {Make} {Model}.");
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -7,6 +8,7 @@ namespace CarLotSimulator
         static void Main(string[] args)
         {
             //TODO
+            var lot = new CarLot();
 
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
@@ -22,12 +24,50 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            var myCar = new Car();
+            myCar.Year = 2022;
+            myCar.Make = "Hyundai";
+            myCar.Model = "Elantra";
+            myCar.EngineNoise = "whir";
+            myCar.HonkNoise = "beep";
+            myCar.IsDriveable = true;
+            lot.Cars.Add(myCar);
+
+            myCar.MakeEngineNoise(myCar.EngineNoise);
+            myCar.MakeHonkNoise(myCar.HonkNoise);
+
+            var myTruck = new Car()
+            {
+                Year = 1996,
+                Make = "Chevrolet",
+                Model = "S-10",
+                EngineNoise = "tick",
+                HonkNoise = "Oomp",
+                IsDriveable = true
+            };
+            lot.Cars.Add(myTruck);
+            myTruck.MakeEngineNoise(myTruck.EngineNoise);
+            myTruck.MakeHonkNoise(myTruck.HonkNoise);
+
+            var myOldCar = new Car(1997, "Ford", "Probe", "no", "Yes", false);
+            lot.Cars.Add(myOldCar);
+            myOldCar.MakeEngineNoise(myOldCar.EngineNoise);
+            myOldCar.MakeHonkNoise(myOldCar.HonkNoise);
+
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            Console.WriteLine("The following vehicles are currently on the lot:");
+            Console.WriteLine("------------------------------------------------");
+            foreach (var car in lot.Cars) 
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
+
         }
+
     }
 }
